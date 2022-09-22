dd2689f47f107b856e64d6c4f1350af6751b6992
Z3RO-O <vaibhavsingh_21523@aitpune.edu.in>
2022-09-22 21:11:19 +0530

    tracks

---
+++ js/fare_calc.js
@@ -0,0 +1,79 @@
+function fare_calculation(D,p1,bw){
+  var totalprice = 10;
+  var bgprice = 10;
+  if (bw <= 10) {bgprice = 10;}
+  else if (bw <= 20 && bw > 11) {bgprice = 20;}
+  else {
+    bgprice = 30;
+  }
+  if (p1 == 1 && D == 1) {
+    totalprice += bgprice;
+  } else if (p1 == 6 && D == 6) {
+    totalprice += bgprice;
+  } else {
+    var diff = p1 - D;
+    if (diff < 0) diff = -1 * diff;
+    totalprice = 15 * diff + bgprice;
+  }
+  
+  return totalprice;
+
+}
+
+
+document.getElementById("result").onclick=function(){
+  var D = document.getElementById("default-select2").value;
+  var p1 = document.getElementById("default-select1").value;
+  var bw = document.getElementById("default-select").value;
+  document.getElementById("result").innerText="₹" + " " + fare_calculation(D,p1,bw);
+ 
+}
+var text=document.querySelector("#txt");
+var text1=document.querySelector("#txt1");
+var qr=document.querySelector("#qr");
+var form1=document.querySelector("#form1");
+var result=document.querySelector("#result");
+
+text.style.display="none";
+result.style.display="none"
+qr.style.display="none";
+document.getElementById("BOOK").onclick=function(){
+    text.style.display="inline";
+    text1.style.display="none";
+    qr.style.display="inline";
+    form1.style.display="none";
+    result.style.display="inline";
+   
+
+
+};
+
+
+/* var D = document.getElementById("default-select2").value;
+var p1 = document.getElementById("default-select1").value;
+var bw = document.getElementById("default-select").value;
+console.log(D);
+console.log(p1);
+console.log(bw);
+var totalprice = 10;
+var bgprice = 10;
+if (bw <= 10) bgprice = 10;
+else if (bw <= 20 && bw > 11) bgprice = 20;
+else {
+  bgprice = 30;
+}
+if (p1 == 1 && D == 1) {
+  totalprice += bgprice;
+} else if (p1 == 6 && D == 6) {
+  totalprice += bgprice;
+} else {
+  var diff = p1 - D;
+  if (diff < 0) diff = -1 * diff;
+  totalprice = 15 * diff + bgprice;
+}
+function fare_display() {
+  var x = totalprice;
+  document.getElementById("BOOK").innerText = x;
+}
+console.log(totalprice);
+ */
---
+++ track.js
@@ -0,0 +1,30 @@
+function trackcoolie(platform){
+    if(platform==1){
+        coolie.style.display="inline";
+        pass.style.display="inline"; 
+    }
+    else if(platform==2){
+        coolie2.style.display="inline";
+        pass2.style.display="inline";
+
+    }
+    else{
+        alert("Enter right platform");
+    }
+}
+
+var coolie=document.querySelector("#dot1");
+var pass=document.querySelector("#dot2");
+var coolie2=document.querySelector("#dot3");
+var pass2=document.querySelector("#dot4");
+coolie.style.display="none";
+pass.style.display="none";
+coolie2.style.display="none";
+pass2.style.display="none";
+document.getElementById("result").onclick=function(){
+    var platform=document.getElementById("pt1").value;
+    trackcoolie(platform);
+    
+    
+  
+};
